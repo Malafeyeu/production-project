b41e8d7eec9dbcee6a098734529bfcf089332f43
MOLOFEEVilya <m_molo@mail.ru>
2024-01-01 22:06:06 +0300

    add config

---
+++ config/build/buildDevServers.ts
@@ -0,0 +1,12 @@
+import { BuildOptions } from './types/config';
+import type { Configuration as DevServerConfiguration} from 'webpack-dev-server';
+
+
+export function buildDevServers(options: BuildOptions): DevServerConfiguration {
+
+  return {
+    port: options.port,
+    open: true,
+    historyApiFallback: true,
+  }
+}
